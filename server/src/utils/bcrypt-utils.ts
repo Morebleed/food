6f1a82fd74e549943070f14774474b1663dff1e9
Morebleed <deegiidk@gmail.com>
2025-05-21 12:33:50 +0800

    21may

--- server/src/utils/bcrypt-utils.ts
+++ server/src/utils/bcrypt-utils.ts
@@ -1,1 +1,9 @@
-import { encryptHash } from "bcryptjs";
+import { hashSync, compareSync } from "bcryptjs";
+
+export const encryptHash = (password: string) => {
+  return hashSync(password, 10);
+};
+
+export const decryptHash = (hash: string, password: string) => {
+  return compareSync(hash, password);
+};
